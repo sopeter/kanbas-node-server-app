aa416fc7a3d1685f44f72151e8776fc8ad0c8062
Peter So <peterjso23@gmail.com>
2024-03-25 20:59:39 -0400

    a5 complete

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
+import CourseRoutes from './Kanbas/courses/routes.js';
+import ModuleRoutes from './Kanbas/modules/routes.js';
 import cors from "cors";
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+ModuleRoutes(app);
+CourseRoutes(app);
 Hello(app);
 Lab5(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
